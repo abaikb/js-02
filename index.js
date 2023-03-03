16ad4f5a3a2f7676eacc77afe85bbc53291e35c6
abaikb <abaikb@icloud.com>
2023-03-03 12:09:39 +0600

    js02

---
+++ index.js
@@ -0,0 +1,77 @@
+/*const x = 12
+console.log(x > 10 && x < 15 && x % 2 === 0)*/
+
+/*const x = 8
+console.log(x % 5 === 0 || x % 3 === 0)*/
+
+/*const x = 8
+x === 10 || console.log('10')*/
+
+/*const x = 7
+console.log(x !== 7)
+console.log(!(x === 7))*/
+
+/*const x = 7
+console.log(x !== 10 && (x > 500 || x < 50))*/
+
+
+/*const x = 40
+console.log(x !== 40 && (x % 5 === 0 || x % 7 === 0))*/
+
+/*const x = 10
+if (x ==2 ) {
+    alert("hello")
+    console.log("hi")
+}*/
+
+/*const name = prompt('ваше имя ?')
+const age = prompt("ваш возраст?")
+
+if (age >= 18) {
+    console.log(' привет, ', name)
+    console.log(`privet, ${name}`)
+}*/
+
+/*const x = 3
+
+
+if (x === 2) {
+    alert('1')
+} else {
+    alert('2')
+}*/
+
+
+/*const age = prompt("ваш возраст?")
+const name = prompt('ваше имя ?')
+
+if (age >= 18) {
+    console.log(' привет, ', name)
+} else {
+    console.log('доступ запрешён!')
+}*/
+
+/*const число = prompt("Введите число !")
+
+if (число % 2 === 0) {
+    console.log('четное')
+} else {
+    console.log('не четное')
+}*/
+
+
+/*const color = 'red'
+
+
+
+if (color === 'red' || color === 'Red') {
+    console.log('stop!')
+} else if (color === yellow) {
+    console.log('warning')
+} else if ( color === 'green') {
+    console.log('go')
+} else {
+    console.log('wrong color')
+}*/
+
+
